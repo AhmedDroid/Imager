684067e66c34bce040cecc85af125792a91d687c
AhmeDroid <a.awad@classera.com>
2020-11-03 23:49:53 +0200

    added base url

--- app/src/main/java/com/ahmedroid/imager/Imager.kt
+++ app/src/main/java/com/ahmedroid/imager/Imager.kt
@@ -1,9 +1,11 @@
 package com.ahmedroid.imager
 
 import android.app.Application
+import com.ahmedroid.data.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class Imager : Application() {
 
+    
 }
