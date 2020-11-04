bd4b63b2557489d0c3154df6fd7bfb5284b437db
AhmeDroid <a.awad@classera.com>
2020-11-05 00:19:10 +0200

    added fetching photo logic into viewmodel

--- UI/src/main/java/com/ahmedroid/ui/main/ImageBrowserViewModel.kt
+++ UI/src/main/java/com/ahmedroid/ui/main/ImageBrowserViewModel.kt
@@ -3,18 +3,54 @@ package com.ahmedroid.ui.main
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import entities.Photo
 import network.Resource
 import repos.PhotosRepo
 
+@Suppress("UNCHECKED_CAST")
 class ImageBrowserViewModel @ViewModelInject constructor(
     private val photosRepo: PhotosRepo
 ): ViewModel() {
 
+    var pageNumber = 0
+    var photosList : List<Photo> = listOf()
+
     fun loadPhotos() = liveData {
         emit(Resource.Loading(show = true))
 
-        val t = photosRepo.getPhotos()
-        emit(t)
+        emit(fetchPhotos())
+
+        incrementPageNumber()
+
         emit(Resource.Loading(show = false))
+    }
+
+    private suspend fun fetchPhotos() : Resource {
+        val photosResult = photosRepo.getPhotosWithPage(pageNumber)
+        if (photosResult is Resource.Success<*>) {
+            photosList = photosResult.data as? List<Photo> ?: listOf()
+        }
+
+        return photosResult
+    }
+
+    private fun incrementPageNumber() {
+        pageNumber++
+    }
+
+    fun refreshPhotos() = liveData {
+        emit(Resource.Loading(show = true))
+
+        resetPageNumber()
+
+        emit(fetchPhotos())
+
+        incrementPageNumber()
+
+        emit(Resource.Loading(show = false))
+    }
+
+    private fun resetPageNumber() {
+        pageNumber = 0
     }
 }
